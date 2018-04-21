d7467584dc78be3ede8792ee23cbe0d2ed9d28af
PatrickWumbo <jhuh009@ucr.edu>
2018-04-21 16:39:34 -0700

    added some possible time functions

--- background.js
+++ background.js
@@ -10,6 +10,50 @@ chrome.runtime.onInstalled.addListener(function() {
   });
 });
 
+// chrome.runtime.onMessage.addListener(function(request) {
+//     if (request.type === 'request_password') {
+//         chrome.tabs.create({
+//             url: chrome.extension.getURL('dialog.html'),
+//             active: false
+//         }, function(tab) {
+//             // After the tab has been created, open a window to inject the tab
+//             chrome.windows.create({
+//                 tabId: tab.id,
+//                 type: 'popup',
+//                 focused: true
+//                 // incognito, top, left, ...
+//             });
+//         });
+//     }
+// });
+// function setPassword(password) {
+//     // Do something, eg..:
+//     console.log(password);
+// };
+
+// function startTimer(duration, display) {
+//     var timer = duration, minutes, seconds;
+//     setInterval(function () {
+//         minutes = parseInt(timer / 60, 10)
+//         seconds = parseInt(timer % 60, 10);
+
+//         minutes = minutes < 10 ? "0" + minutes : minutes;
+//         seconds = seconds < 10 ? "0" + seconds : seconds;
+
+//         display.textContent = minutes + ":" + seconds;
+
+//         if (--timer < 0) {
+//             timer = duration;
+//         }
+//     }, 1000);
+// }
+
+// window.onload = function () {
+//     var fiveMinutes = 60 * 5,
+//         display = document.querySelector('#time');
+//     startTimer(fiveMinutes, display);
+// };
+
 var timeSet = 1000;
 
 
