945ed1df517d79680f592ad7a7b65f3fcf414ba2
PatrickWumbo <jhuh009@ucr.edu>
2018-04-21 17:21:04 -0700

    setting up for dialog box

--- background.js
+++ background.js
@@ -31,38 +31,38 @@ chrome.runtime.onInstalled.addListener(function() {
 //     console.log(password);
 // };
 
-// function startTimer(duration, display) {
-//     var timer = duration, minutes, seconds;
-//     setInterval(function () {
-//         minutes = parseInt(timer / 60, 10)
-//         seconds = parseInt(timer % 60, 10);
+function startTimer(duration, display) {
+    var timer = duration, minutes, seconds;
+    setInterval(function () {
+        minutes = parseInt(timer / 60, 10)
+        seconds = parseInt(timer % 60, 10);
 
-//         minutes = minutes < 10 ? "0" + minutes : minutes;
-//         seconds = seconds < 10 ? "0" + seconds : seconds;
+        minutes = minutes < 10 ? "0" + minutes : minutes;
+        seconds = seconds < 10 ? "0" + seconds : seconds;
 
-//         display.textContent = minutes + ":" + seconds;
+        display.textContent = minutes + ":" + seconds;
 
-//         if (--timer < 0) {
-//             timer = duration;
-//         }
-//     }, 1000);
-// }
+        if (--timer < 0) {
+            timer = duration;
+        }
+    }, 1000);
+}
 
-// window.onload = function () {
-//     var fiveMinutes = 60 * 5,
-//         display = document.querySelector('#time');
-//     startTimer(fiveMinutes, display);
-// };
+window.onload = function () {
+    var fiveMinutes = 60 * 5,
+        display = document.querySelector('#time');
+    startTimer(fiveMinutes, display);
+};
 
-var timeSet = 1000;
+// var timeSet = 1000;
 
 
 
-var userTime = setTimeout(alertUser, timeSet);
+// var userTime = setTimeout(alertUser, timeSet);
 
-function dialogbox() {
-    alert( "#dialog" ).dialog();
- }
+// function dialogbox() {
+//     alert( "#dialog" ).dialog();
+//  }
 
 
 // function alertUser() {
