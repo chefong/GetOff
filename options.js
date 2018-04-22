6bc803425f20bb1cc4fe0e0139f316d564393bdd
bball24 <jerry88tan@gmail.com>
2018-04-21 23:00:03 -0700

    Added bootstrap

--- options.js
+++ options.js
@@ -1,32 +1,29 @@
-// Saves options to chrome.storage
-function save_options() {
-  var color = document.getElementById('color').value;
-  var likesColor = document.getElementById('like').checked;
+// Copyright 2018 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+function saveDefaultTimer() {
+  console.log(document.getElementById("Hours").value);
+  console.log(document.getElementById("Minutes").value);
+  while (document.getElementById("Hours").value < 0){
+    alert("please enter a number 0 or greater");
+  }
+  while (document.getElementById("Minutes").value < 0 || document.getElementById('Minutes').value > 60 ){
+    alert("please enter a number 0 or greater but no more than 60");
+  }
+  var defualtHours = document.getElementById("Hours").value;
+  var defaultMinutes = document.getElementById("Minutes").value;
   chrome.storage.sync.set({
-    favoriteColor: color,
-    likesColor: likesColor
+    savedDefualtHours: defualtHours
+    savedDefualtMinutes: defaultMinutes
   }, function() {
     // Update status to let user know options were saved.
-    var status = document.getElementById('status');
-    status.textContent = 'Options saved.';
+    var status = document.getElementById("status");
+    status.textContent = "Options saved.";
     setTimeout(function() {
       status.textContent = '';
     }, 750);
   });
 }
 
-// Restores select box and checkbox state using the preferences
-// stored in chrome.storage.
-function restore_options() {
-  // Use default value color = 'red' and likesColor = true.
-  chrome.storage.sync.get({
-    favoriteColor: 'red',
-    likesColor: true
-  }, function(items) {
-    document.getElementById('color').value = items.favoriteColor;
-    document.getElementById('like').checked = items.likesColor;
-  });
-}
-document.addEventListener('DOMContentLoaded', restore_options);
-document.getElementById('save').addEventListener('click',
-    save_options);
+document.getElementById("save").addEventListener("click", saveDefualtTimer, false);
