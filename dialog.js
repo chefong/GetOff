633364504f762c7ad887bb45ab63b3396b11b729
PatrickWumbo <jhuh009@ucr.edu>
2018-04-21 18:03:12 -0700

    ree

--- dialog.js
+++ dialog.js
@@ -1,8 +1,22 @@
-document.forms[0].onsubmit = function(e) {
-    e.preventDefault(); // Prevent submission
-    var password = document.getElementById('pass').value;
-    chrome.runtime.getBackgroundPage(function(bgWindow) {
-        bgWindow.setPassword(password);
-        window.close();     // Close dialog
-    });
+function startTimer(duration, display) {
+    var timer = duration, minutes, seconds;
+    setInterval(function () {
+        minutes = parseInt(timer / 60, 10)
+        seconds = parseInt(timer % 60, 10);
+
+        minutes = minutes < 10 ? "0" + minutes : minutes;
+        seconds = seconds < 10 ? "0" + seconds : seconds;
+
+        display.textContent = minutes + ":" + seconds;
+
+        if (--timer < 0) {
+            timer = duration;
+        }
+    }, 1000);
+}
+
+window.onload = function () {
+    var fiveMinutes = 60 * 5,
+        display = document.querySelector('#time');
+    startTimer(fiveMinutes, display);
 };
